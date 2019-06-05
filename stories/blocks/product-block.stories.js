70962f292e9b75becbad459e4a9f72685de6a1ee
Kamil <kmirosz6@gmail.com>
2019-06-05 15:03:22 +0200

    changes

--- stories/blocks/product-block.stories.js
+++ stories/blocks/product-block.stories.js
@@ -5,25 +5,12 @@ import '../../resources/assets/styles/main.scss';
 import sofa from '../../resources/assets/images/sofa.jpg'
 
 const productBlock = `
-<div class="product-block" href="">
-    <a class="product-block__header">
-        <h3 class="product-block__title subtitle light">
-            Sofa white
-        </h3>
-        <span class="title bold">
-            976<span class="subtitle bold">.75 </span>
-        </span>
-    </a>
-    <img class="product-block__image" src="${sofa}" alt="Title">
-    <div class="product-block__footer">
-        <a href="">
-            <span class="icon icon--plus icon--dark"></span>
-        </a>
-    </div>
-</div>`;
+<button class="button">
+    Hello
+</button>`;
 
-storiesOf('Blocks', module)
-    .add('Product block', () => `
+storiesOf('Elementy/przyciski', module)
+    .add('Button', () => `
     <h2 class="subtitle bold">Product block</h2>
     <hr>
     <br>
