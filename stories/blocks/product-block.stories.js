88d9fd52e13f71fdd227866ac317e30ce5ec6315
Kamil <kmirosz6@gmail.com>
2019-06-05 15:17:59 +0200

    icon example

--- stories/blocks/product-block.stories.js
+++ stories/blocks/product-block.stories.js
@@ -5,9 +5,16 @@ import '../../resources/assets/styles/main.scss';
 import sofa from '../../resources/assets/images/sofa.jpg'
 
 const productBlock = `
-<button class="button">
-    Hello
-</button>`;
+<div class="icon-box">
+    <img class="icon-box__icon" src="${sofa}" alt="Sofa">
+    <p class="icon-box__dsc text bold"> Lorem ipsum </p>
+</div>`;
+
+const IconBoxCenter = `
+<div class="icon-box icon-box--center">
+    <img class="icon-box__icon" src="${sofa}" alt="Sofa">
+    <p class="icon-box__dsc text bold"> Lorem ipsum </p>
+</div>`;
 
 storiesOf('Elementy/przyciski', module)
     .add('Button', () => `
@@ -20,6 +27,21 @@ storiesOf('Elementy/przyciski', module)
     <br>
     <xmp>
         ${productBlock}
+    </xmp>
+
+  `);
+
+  storiesOf('Elementy/przyciski', module)
+    .add('icon center', () => `
+    <h2 class="subtitle bold">Product block</h2>
+    <hr>
+    <br>
+    <br>
+    ${IconBoxCenter}
+    <br>
+    <br>
+    <xmp>
+        ${IconBoxCenter}
     </xmp>
 
   `);
