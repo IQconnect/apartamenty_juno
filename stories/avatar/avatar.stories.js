d6615c2283e2d904d9e684ed913cdfdb34c8def0
Robert <robert.m93a@gmail.com>
2019-06-07 13:57:49 +0200

    Avatar 2 Naprawa kompletna

--- stories/avatar/avatar.stories.js
+++ stories/avatar/avatar.stories.js
@@ -1,26 +1,29 @@
-import { storiesOf } from '@storybook/html';
+import { storiesOf } from "@storybook/html";
 
-import '../../resources/assets/styles/main.scss';
+import "../../resources/assets/styles/main.scss";
 
-import sofa from '../../resources/assets/images/sofa-bg.jpg'
+import sofa from "../../resources/assets/images/sofa-bg.jpg";
 
 const Avatar = `
 <div class="avatar">
     <img src="${sofa}" class="avatar--image--cover">
-    <p class="avatar--name text main text--thin text--left">
+    <p class="avatar--name text main text--thin text text--left">
     Pani kanapa
     </p>
-    <br/>
-    <p><a href="#" class="avatar--lighter text small-text ">
+    <p>
+    <a href="#" class="avatar__link text small-text ">
     tel: +48 500 526 600
-    </a></p>
-    <p><a href="#"  class="avatar--lighter text small-text ">
+    </a>
+    <br/>
+    <a href="#"  class="avatar__link text small-text ">
     mail: biuro@apartamentyjuno.pl
-    </a></p>
+    </a>
+    </p>
 </div>`;
 
-  storiesOf('Elementy/avatar', module)
-    .add('avatar', () => `
+storiesOf("Elementy/avatar", module).add(
+  "avatar",
+  () => `
     <h2 class="subtitle bold">Avatar</h2>
     <hr>
     <br>
@@ -32,6 +35,7 @@ const Avatar = `
     ${Avatar}
     </xmp>
 
-  `);
+  `
+);
 
 export default Avatar;
