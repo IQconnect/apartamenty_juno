f75b40a8c1ce1869bab9d75cfa274fe8ac4b4fca
Kamil <kmirosz6@gmail.com>
2019-06-07 13:39:43 +0200

    fix header

--- stories/blocks/header-block.stories.js
+++ stories/blocks/header-block.stories.js
@@ -1,80 +1,69 @@
 import { storiesOf } from '@storybook/html';
 
 import '../../resources/assets/styles/main.scss';
-import boat from  '../../resources/assets/images/boat.png';
+import boat from '../../resources/assets/images/boat.png';
+
+const sectionHeader = `
+  <h2 class="section-header">
+    <span class="section-header__title title text--center">
+      Doskonała lokalizacja
+    </span>
+    <span class="section-header__sub subtitle text--center">
+      Wysoki standard
+    </span>
+  </h2>
+`;
+
+const HeaderSpecial = `
+  <div class="section-header">
+    <img class="section-header__img" src="${boat}">
+    <h2 class="section-header__title title text--center">
+      Doskonała lokalizacja
+    </h2>
+  </div>
+`;
+
+const HeaderLeft = `
+  <h2 class="section-header">
+    <span class="title section-header__title--left">
+      Dlaczego warto
+    </span>
+    <span class="section-header__sub--left subtitle">
+      tu zamieszkać?
+    </span>
+  </h2>
+`;
 
 storiesOf('Elementy/header', module)
-    .add('Header', () => `
+  .add('Header', () => `
     <h2 class="subtitle">
       Header
     </h2>
     <hr />
-    <div class="header">
-      <h2>
-        <span class="header__title title text--center">
-          Doskonała lokalizacja
-        </span>
-        <span class="header__sub subtitle text--center">
-          Wysoki standard
-        </span>
-      </h2>
-    </div>
+    ${sectionHeader}
     <xmp>
-      <div class="header">
-        <h2>
-          <span class="header__title title text--center">
-            Doskonała lokalizacja
-          </span>
-          <span class="header__sub subtitle text--center">
-            Wysoki standard
-          </span>
-        </h2>
-      </div>
+      ${sectionHeader}
     </xmp>
     <br><br>
     <h2 class="subtitle">
       Header Special
     </h2>
     <hr />
-    <div class="header">
-      <img class="header__img" src="${boat}">
-      <h2 class="header__title title text--center">
-        Doskonała lokalizacja
-      </h2>
-    </div>
+      ${HeaderSpecial}
     <xmp>
-      <div class="header">
-        <img class="header__img" src="${boat}">
-        <h2 class="header__title title text--center">
-          Doskonała lokalizacja
-        </h2>
-      </div>
+      ${HeaderSpecial}
     </xmp>
     <br><br>
     <h2 class="subtitle">
       Header Left
     </h2>
     <hr />
-    <div class="header">
-      <h2>
-        <span class="title header__title--left">
-          Dlaczego warto
-        </span>
-        <span class="header__sub--left subtitle">
-          tu zamieszkać?
-        </span>
-      </h2>
+    <div style="background-color: #364ea2; padding: 20px;">
+      ${HeaderLeft}
     </div>
     <xmp>
-    <div class="header">
-      <h2>
-        <span class="title header__title--left">
-          Dlaczego warto
-        </span>
-        <span class="header__sub--left subtitle">
-          tu zamieszkać?
-        </span>
-      </h2>
+    <div class="section-header">
+      ${HeaderLeft}
     </div>
     </xmp>
     <br><br>
