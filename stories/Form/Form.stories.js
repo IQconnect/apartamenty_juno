9c93f7d65e27fee60189d00fb099b48be8240a68
Robert <robert.m93a@gmail.com>
2019-06-10 08:34:10 +0200

    input 1

--- stories/Form/Form.stories.js
+++ stories/Form/Form.stories.js
@@ -1,28 +1,45 @@
-import { storiesOf } from '@storybook/html';
+import { storiesOf } from "@storybook/html";
 
-import '../../resources/assets/styles/main.scss';
-
-
+import "../../resources/assets/styles/main.scss";
 
 const Input = `
-<div class="input">
-  <p><label for="name">Name:</label></p>
-  <input type="text" name="name" id="name" placeholder="John Smith" />
-</div>`;
+  <p>
+  <label class="input__label text text--thin" for="name">Imie i Nazwisko</label></p>
+  <input class="input" type="text" name="name" id="name" placeholder="" />
+`;
+const Message = `
+  <p>
+  <label class="input__label text text--thin" for="name">
+ Wiadomość</label>
+  </p>
+  <input class="input input__message" type="text" name="name" id="name" placeholder="" />
+`;
 
-  storiesOf('Elementy/Form', module)
-    .add('Input', () => `
+storiesOf("Elementy/Form", module).add(
+  "Input",
+  () => `
     <h2 class="subtitle bold">Input</h2>
     <hr>
     <br>
     <br>
-    ${ Input}
+    ${Input}
     <br>
     <br>
     <xmp>
-        ${ Input}
+    ${Input}
+    </xmp>
+    <h2 class="subtitle bold">Message</h2>
+    <hr>
+    <br>
+    <br>
+    ${Message}
+    <br>
+    <br>
+    <xmp>
+    ${Message}
     </xmp>
 
-  `);
+  `
+);
 
-export default  Input;
+export default Input;
