d45b8246e6c72ed36983571a508aa76cfe0e4f6a
ralph <rafal@creativeroom.pl>
2019-06-11 09:07:03 +0200

    footer

--- stories/layouts/footer.stories.js
+++ stories/layouts/footer.stories.js
@@ -3,12 +3,56 @@ import { storiesOf } from '@storybook/html';
 import '../../resources/assets/styles/main.scss';
 import bg from '../../resources/assets/images/footer.png';
 import logo from '../../resources/assets/images/footer-logo.png';
+import iq from '../../resources/assets/images/iq.png';
 
 const footer = `
-<footer class="footer overlay">
+<footer class="footer">
   <img class="footer__bg" src="${bg}" alt="background">
-  <div class="footer__content">
-    <img class="footer__logo" src="${logo}" alt="logo">
+  <div class="container">
+    <div class="footer__content">
+      <div class="footer__left">
+        <img class="footer__logo" src="${logo}" alt="logo">
+        <nav>
+          <ul class="footer__nav">
+            <li>
+              <a href="#" class="small-text footer__link">
+                O inwestycji
+              </a>
+            </li>
+            <li>
+              <a href="#" class="small-text footer__link">
+                Znajdź mieszkanie
+              </a>
+            </li>
+            <li>
+              <a href="#" class="small-text footer__link">
+                Wizualizacje
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <nav>
+          <ul class="footer__nav">
+            <li>
+              <a href="#" class="small-text footer__link">
+                O inwestycji
+              </a>
+            </li>
+            <li>
+              <a href="#" class="small-text footer__link">
+                Znajdź mieszkanie
+              </a>
+            </li>
+            <li>
+              <a href="#" class="small-text footer__link">
+                Wizualizacje
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <img class="footer__by" src="${iq}" alt="logo">
+    </div>
   </div>
 </footer>
 `;
