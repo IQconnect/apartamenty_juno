1beb52f248184e01c89b1d6c58b601cea95b091e
Kamil <kmirosz6@gmail.com>
2019-10-21 10:33:23 +0200

    Slogan

---
+++ resources/views/layouts/components/slogan.blade.php
@@ -0,0 +1,19 @@
+@php
+    $slogan = $data['slogan'];    
+    $sign = $data['sign'];    
+@endphp
+
+@if ($slogan )
+<section class="section section--shade slogan">
+    <div class="container">
+        <h2 class="slogan__title title main-title">
+            {!! $slogan !!}
+        </h2>
+        @if ($sign)
+        <p class="slogan__sign text">
+            {!! $sign !!}
+        </p>
+        @endif
+    </div>
+</section>
+@endif
